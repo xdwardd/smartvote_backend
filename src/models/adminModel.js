b152e7642e08ed9d6847b4fefb4ead2e0e2f1141
xdwardd <xdwardd@gmail.com>
2025-07-23 23:39:52 +0800

    updated

--- src/models/adminModel.js
+++ src/models/adminModel.js
@@ -1,5 +1,15 @@
 const db = require('../config/db');
 
+const testemails = [
+  {id: 1, email: "joshuacatapan2003@gmail.com"},
+  {id: 2, email: "crmchs.metante.jessriel@gmail.com"},
+  {id: 3, email: "caparosocriety29@gmail.com"},
+  {id: 3, email: "edwardcatapan@gmail.com"},
+ 
+]
+
+const sendEmail = require('../utils/mailer');
+
 const Admin = {
 
 // Open filing of candidacy
@@ -11,8 +21,27 @@ const Admin = {
         _end_date,
         _status,
     ]);
-    return rows[0][0]; // stored procedure returns array of arrays
- },  
+   const result = rows[0][0];
+   // return rows[0][0]; // stored procedure returns array of arrays
+   // Check if email should be sent
+        if (result.retVal === 1) {
+        await sendEmail({
+            to: testemails.map((e) => e.email), // ✅ extract email addresses
+            text: `Hello ,\n\nThe filing of candidacy is now officially open.\n\nIf you believe you have the qualities to lead and serve, we highly encourage you to file your candidacy within the given deadline.\n\nThank you.`,
+            html: `
+              <p>Hello</p>
+              <p>The <strong>filing of candidacy</strong> is now officially open.</p>
+              <p>If you believe you have the qualities to lead and serve, we highly encourage you to file your candidacy within the given deadline.</p>
+              <p>Thank you.</p>
+                      `
+
+        });
+        }
+
+         return result;
+        
+ 
+  },  
 
  getFilingStatus: async() => {
   const [rows] = await db.execute('CALL GetFilingStatus()');
--- src/models/candidatesModel.js
+++ src/models/candidatesModel.js
@@ -14,20 +14,20 @@ const Candidates = {
         const result = rows[0][0];
 
         // Check if email should be sent
-        // if (result.retVal === 1) {
-        // await sendEmail({
-        //     to: 'edwardcatapan@gmail.com',  //Admin Emails
-        //     subject: 'COC Submission',
-        //     text: `Hello Admin,\n\n${_name} has submitted an application for the position of ${_position}.\n\nPlease review the submission at your earliest convenience.\n\nThank you.`,
-        //     html: `
-        //     <p>Hello Admin,</p>
-        //     <p><strong>${_name}</strong> has submitted an application for the position of <strong>${_position}</strong>.</p>
-        //     <p>Please review the submission at your earliest convenience.</p>
-        //     <p>Thank you.</p>
-        //     `
+        if (result.retVal === 1) {
+        await sendEmail({
+            to: 'joshuacatapan2003@gmail.com',  //Admin Emails
+            subject: 'COC Submission',
+            text: `Hello Admin,\n\n${_name} has submitted an application for the position of ${_position}.\n\nPlease review the submission at your earliest convenience.\n\nThank you.`,
+            html: `
+            <p>Hello Admin,</p>
+            <p><strong>${_name}</strong> has submitted an application for the position of <strong>${_position}</strong>.</p>
+            <p>Please review the submission at your earliest convenience.</p>
+            <p>Thank you.</p>
+            `
 
-        // });
-        // }
+        });
+        }
 
          return result;
         
